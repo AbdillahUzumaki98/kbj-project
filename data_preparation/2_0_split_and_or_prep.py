ccf7ca16a0776f423dbc50ee48fbf8c6e4425a4f
Rudy Rachman <rudyrachman16@gmail.com>
2023-05-30 18:29:36 +0800

    update Size 256

--- data_preparation/2_0_split_and_or_prep.py
+++ data_preparation/2_0_split_and_or_prep.py
@@ -14,11 +14,13 @@ source_path = "1_2_aug_included" if is_aug else "1_1_image_only"
 source_dirs = os.listdir(source_path)
 
 dest_path = "2_0_ready"
-dest_prep_path = "2_1_swt-2"
-dest_y_cb_cr_path = "2_2_y_cb_cr"
-dest_y_cb_cr_prep_path = "2_3_y_cb_cr_prep"
+dest_swt_2_path = "2_1_swt-2"
+dest_dwt_path = "2_2_dwt"
+dest_y_cb_cr_path = "2_3_y_cb_cr"
+dest_y_cb_cr_swt_2_path = "2_4_y_cb_cr_swt-2"
+dest_y_cb_cr_dwt_path = "2_5_y_cb_cr_dwt"
 
-IMAGE_SIZE = (400, 400)
+IMAGE_SIZE = (256, 256)
 
 
 def check_or_clear_folder(path, is_need_clear):
@@ -34,9 +36,11 @@ def check_or_clear_folder(path, is_need_clear):
 
 
 check_or_clear_folder(dest_path, is_need_clear=True)
-check_or_clear_folder(dest_prep_path, is_need_clear=True)
+check_or_clear_folder(dest_swt_2_path, is_need_clear=True)
+check_or_clear_folder(dest_dwt_path, is_need_clear=True)
 check_or_clear_folder(dest_y_cb_cr_path, is_need_clear=True)
-check_or_clear_folder(dest_y_cb_cr_prep_path, is_need_clear=True)
+check_or_clear_folder(dest_y_cb_cr_swt_2_path, is_need_clear=True)
+check_or_clear_folder(dest_y_cb_cr_dwt_path, is_need_clear=True)
 
 
 
@@ -50,36 +54,63 @@ def swt(prep_im):
 
 
 
+def dwt(prep_im):
+    copy_im = prep_im.copy()
+    dest_area = IMAGE_SIZE[0] * IMAGE_SIZE[1]
+    source_area = copy_im.shape[0] * copy_im.shape[1]
+    copy_im = cv2.resize(copy_im, (IMAGE_SIZE[0], IMAGE_SIZE[1]), interpolation=cv2.INTER_AREA if source_area > dest_area else cv2.INTER_CUBIC)
+    for i in range(3):
+        c = prep_im[:, :, i]  # take each channel and after that do wavelet
+        levels = pywt.dwt2(c, "haar")
+        approx, _ = levels  # approximation, (horizontal, vertical, diagonal)
+        copy_im[:, :, i] = approx
+    return copy_im
+
+
+
 def copy_data(data, data_type, label_name):
     dest_full_path = os.path.join(dest_path, data_type, label_name)
-    dest_prep_full_path = os.path.join(dest_prep_path, data_type, label_name)
+    dest_swt_2_full_path = os.path.join(dest_swt_2_path, data_type, label_name)
+    dest_dwt_full_path = os.path.join(dest_dwt_path, data_type, label_name)
     dest_y_cb_cr_full_path = os.path.join(dest_y_cb_cr_path, data_type, label_name)
-    dest_y_cb_cr_prep_full_path = os.path.join(dest_y_cb_cr_prep_path, data_type, label_name)
+    dest_y_cb_cr_swt_2_full_path = os.path.join(dest_y_cb_cr_swt_2_path, data_type, label_name)
+    dest_y_cb_cr_dwt_full_path = os.path.join(dest_y_cb_cr_dwt_path, data_type, label_name)
+    
     check_or_clear_folder(dest_full_path, is_need_clear=False)
-    check_or_clear_folder(dest_prep_full_path, is_need_clear=False)
+    check_or_clear_folder(dest_swt_2_full_path, is_need_clear=False)
+    check_or_clear_folder(dest_dwt_full_path, is_need_clear=False)
     check_or_clear_folder(dest_y_cb_cr_full_path, is_need_clear=False)
-    check_or_clear_folder(dest_y_cb_cr_prep_full_path, is_need_clear=False)
+    check_or_clear_folder(dest_y_cb_cr_swt_2_full_path, is_need_clear=False)
+    check_or_clear_folder(dest_y_cb_cr_dwt_full_path, is_need_clear=False)
 
     index = random.randint(0, len(data) - 1)
     file_path = os.path.join(source_path, label_name, data[index])
-    im = io.imread(file_path)
-    im = cv2.cvtColor(im, cv2.COLOR_RGBA2BGR)
+    orig_im = io.imread(file_path)
+    orig_im = cv2.cvtColor(orig_im, cv2.COLOR_RGBA2BGR)
     
     dest_area = IMAGE_SIZE[0] * IMAGE_SIZE[1]
-    source_area = im.shape[0] * im.shape[1]
-    im = cv2.resize(im, (IMAGE_SIZE[0], IMAGE_SIZE[1]), interpolation=cv2.INTER_AREA if source_area > dest_area else cv2.INTER_CUBIC)
+    source_area = orig_im.shape[0] * orig_im.shape[1]
+    im = cv2.resize(orig_im, (IMAGE_SIZE[0], IMAGE_SIZE[1]), interpolation=cv2.INTER_AREA if source_area > dest_area else cv2.INTER_CUBIC)
     cv2.imwrite(os.path.join(dest_full_path, data[index]), im)
 
-    prep_im = im.copy()
-    prep_im = swt(prep_im)
-    cv2.imwrite(os.path.join(dest_prep_full_path, data[index]), prep_im)
+    swt_2_im = im.copy()
+    swt_2_im = swt(swt_2_im)
+    cv2.imwrite(os.path.join(dest_swt_2_full_path, data[index]), swt_2_im)
+
+    dwt_im = orig_im.copy()
+    dwt_im = dwt(dwt_im)
+    cv2.imwrite(os.path.join(dest_dwt_full_path, data[index]), dwt_im)
 
     y_cb_cr = cv2.cvtColor(im, cv2.COLOR_BGR2YCR_CB)
     cv2.imwrite(os.path.join(dest_y_cb_cr_full_path, data[index]), y_cb_cr)
 
-    y_cb_cr_prep = y_cb_cr.copy()
-    y_cb_cr_prep = swt(y_cb_cr_prep)
-    cv2.imwrite(os.path.join(dest_y_cb_cr_prep_full_path, data[index]), y_cb_cr_prep)
+    y_cb_cr_swt_2 = y_cb_cr.copy()
+    y_cb_cr_swt_2 = swt(y_cb_cr_swt_2)
+    cv2.imwrite(os.path.join(dest_y_cb_cr_swt_2_full_path, data[index]), y_cb_cr_swt_2)
+
+    y_cb_cr_dwt = cv2.cvtColor(orig_im, cv2.COLOR_BGR2YCR_CB)
+    y_cb_cr_dwt = dwt(y_cb_cr_dwt)
+    cv2.imwrite(os.path.join(dest_y_cb_cr_dwt_full_path, data[index]), y_cb_cr_dwt)
 
     data.pop(index)
 
--- data_preparation/2_1_split_and_or_prep_per_items.py
+++ data_preparation/2_1_split_and_or_prep_per_items.py
@@ -13,11 +13,13 @@ source_path = "1_2_aug_included" if is_aug else "1_1_image_only"
 source_dirs = os.listdir(source_path)
 
 dest_path = "2_0_ready"
-dest_prep_path = "2_1_swt-2"
-dest_y_cb_cr_path = "2_2_y_cb_cr"
-dest_y_cb_cr_prep_path = "2_3_y_cb_cr_prep"
+dest_swt_2_path = "2_1_swt-2"
+dest_dwt_path = "2_2_dwt"
+dest_y_cb_cr_path = "2_3_y_cb_cr"
+dest_y_cb_cr_swt_2_path = "2_4_y_cb_cr_swt-2"
+dest_y_cb_cr_dwt_path = "2_5_y_cb_cr_dwt"
 
-IMAGE_SIZE = (400, 400)
+IMAGE_SIZE = (256, 256)
 
 
 def check_or_clear_folder(path, is_need_clear):
@@ -33,9 +35,11 @@ def check_or_clear_folder(path, is_need_clear):
 
 
 check_or_clear_folder(dest_path, is_need_clear=True)
-check_or_clear_folder(dest_prep_path, is_need_clear=True)
+check_or_clear_folder(dest_swt_2_path, is_need_clear=True)
+check_or_clear_folder(dest_dwt_path, is_need_clear=True)
 check_or_clear_folder(dest_y_cb_cr_path, is_need_clear=True)
-check_or_clear_folder(dest_y_cb_cr_prep_path, is_need_clear=True)
+check_or_clear_folder(dest_y_cb_cr_swt_2_path, is_need_clear=True)
+check_or_clear_folder(dest_y_cb_cr_dwt_path, is_need_clear=True)
 
 img_names = []
 for filename in os.listdir("1_1_image_only/original"):
@@ -53,6 +57,20 @@ def swt(prep_im):
 
 
 
+def dwt(prep_im):
+    copy_im = prep_im.copy()
+    dest_area = IMAGE_SIZE[0] * IMAGE_SIZE[1]
+    source_area = copy_im.shape[0] * copy_im.shape[1]
+    copy_im = cv2.resize(copy_im, (IMAGE_SIZE[0], IMAGE_SIZE[1]), interpolation=cv2.INTER_AREA if source_area > dest_area else cv2.INTER_CUBIC)
+    for i in range(3):
+        c = prep_im[:, :, i]  # take each channel and after that do wavelet
+        levels = pywt.dwt2(c, "haar")
+        approx, _ = levels  # approximation, (horizontal, vertical, diagonal)
+        copy_im[:, :, i] = approx
+    return copy_im
+
+
+
 def copy_data(names, data_type):
     index = random.randint(0, len(names) - 1)
     
@@ -61,13 +79,18 @@ def copy_data(names, data_type):
     
     for label_name in source_dirs:
         dest_full_path = os.path.join(dest_path, data_type, label_name)
-        dest_prep_full_path = os.path.join(dest_prep_path, data_type, label_name)
+        dest_swt_2_full_path = os.path.join(dest_swt_2_path, data_type, label_name)
+        dest_dwt_full_path = os.path.join(dest_dwt_path, data_type, label_name)
         dest_y_cb_cr_full_path = os.path.join(dest_y_cb_cr_path, data_type, label_name)
-        dest_y_cb_cr_prep_full_path = os.path.join(dest_y_cb_cr_prep_path, data_type, label_name)
+        dest_y_cb_cr_swt_2_full_path = os.path.join(dest_y_cb_cr_swt_2_path, data_type, label_name)
+        dest_y_cb_cr_dwt_full_path = os.path.join(dest_y_cb_cr_dwt_path, data_type, label_name)
+        
         check_or_clear_folder(dest_full_path, is_need_clear=False)
-        check_or_clear_folder(dest_prep_full_path, is_need_clear=False)
+        check_or_clear_folder(dest_swt_2_full_path, is_need_clear=False)
+        check_or_clear_folder(dest_dwt_full_path, is_need_clear=False)
         check_or_clear_folder(dest_y_cb_cr_full_path, is_need_clear=False)
-        check_or_clear_folder(dest_y_cb_cr_prep_full_path, is_need_clear=False)
+        check_or_clear_folder(dest_y_cb_cr_swt_2_full_path, is_need_clear=False)
+        check_or_clear_folder(dest_y_cb_cr_dwt_full_path, is_need_clear=False)
 
         filenames = os.listdir(os.path.join(source_path, label_name))
         filtered = []
@@ -77,24 +100,32 @@ def copy_data(names, data_type):
 
         for data in filtered:
             file_path = os.path.join(source_path, label_name, data)
-            im = io.imread(file_path)
-            im = cv2.cvtColor(im, cv2.COLOR_RGBA2BGR)
+            orig_im = io.imread(file_path)
+            orig_im = cv2.cvtColor(orig_im, cv2.COLOR_RGBA2BGR)
             
             dest_area = IMAGE_SIZE[0] * IMAGE_SIZE[1]
-            source_area = im.shape[0] * im.shape[1]
-            im = cv2.resize(im, (IMAGE_SIZE[0], IMAGE_SIZE[1]), interpolation=cv2.INTER_AREA if source_area > dest_area else cv2.INTER_CUBIC)
+            source_area = orig_im.shape[0] * orig_im.shape[1]
+            im = cv2.resize(orig_im, (IMAGE_SIZE[0], IMAGE_SIZE[1]), interpolation=cv2.INTER_AREA if source_area > dest_area else cv2.INTER_CUBIC)
             cv2.imwrite(os.path.join(dest_full_path, data), im)
 
-            prep_im = im.copy()
-            prep_im = swt(prep_im)
-            cv2.imwrite(os.path.join(dest_prep_full_path, data), prep_im)
+            swt_2_im = im.copy()
+            swt_2_im = swt(swt_2_im)
+            cv2.imwrite(os.path.join(dest_swt_2_full_path, data), swt_2_im)
+
+            dwt_im = orig_im.copy()
+            dwt_im = dwt(dwt_im)
+            cv2.imwrite(os.path.join(dest_dwt_full_path, data), dwt_im)
 
             y_cb_cr = cv2.cvtColor(im, cv2.COLOR_BGR2YCR_CB)
             cv2.imwrite(os.path.join(dest_y_cb_cr_full_path, data), y_cb_cr)
 
-            y_cb_cr_prep = y_cb_cr.copy()
-            y_cb_cr_prep = swt(y_cb_cr_prep)
-            cv2.imwrite(os.path.join(dest_y_cb_cr_prep_full_path, data), y_cb_cr_prep)
+            y_cb_cr_swt_2 = y_cb_cr.copy()
+            y_cb_cr_swt_2 = swt(y_cb_cr_swt_2)
+            cv2.imwrite(os.path.join(dest_y_cb_cr_swt_2_full_path, data), y_cb_cr_swt_2)
+
+            y_cb_cr_dwt = cv2.cvtColor(orig_im, cv2.COLOR_BGR2YCR_CB)
+            y_cb_cr_dwt = dwt(y_cb_cr_dwt)
+            cv2.imwrite(os.path.join(dest_y_cb_cr_dwt_full_path, data), y_cb_cr_dwt)
     
     names.pop(index)
 
